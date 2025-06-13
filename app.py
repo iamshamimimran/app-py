7d91f0474a2dda8bc5f47b609ae84ff9ef73b06d
Shamim Imran <94374652+iamshamimimran@users.noreply.github.com>
2025-06-13 17:15:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,4 +43,8 @@ def get_transcript():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port, debug=True)
+
